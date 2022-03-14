2e54237a4ab41afcb72d14bb76a9cf4d3b285008
staten09 <staten09@gmail.com>
2022-03-13 22:28:27 -0500

    Completed TypeScript Exercise

--- exercises/parts1-5.ts
+++ exercises/parts1-5.ts
@@ -1,17 +1,23 @@
+import { SpaceLocation } from './SpaceLocation';
 // URL for the instructions: 
 // https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/exercises.html 
 
 
 // Part 1: Declare (5) Variables With Type
-
-
+let spacecraftName: string;
+let speedMph: number;
+let kilometersToMars: number = 	225000000;
+let kilometersToTheMoon: number = 384400;
+let milesPerKilometer: number;
 
 // Part 2: Print Days to Mars
-
+let milesToMars: number = kilometersToMars * milesPerKilometer;
+let hoursToMars: number = milesToMars / speedMph;
+let daysToMars: number = hoursToMars / 24;
 
 
 // Code an output statement here (use a template literal):
-
+// console.log(`${spacecraftName} would take ${daysToMars} days to get to Mars.`)
 
 
 // Part 3: Create a Function ("getDaysToLocation")
@@ -22,21 +28,33 @@
 // the function and print the outputs for a Mars trip and a moon trip.
 
 
-
-
 // Part 4: Create a Spacecraft Class
+class Spacecraft{
+        milesPerKilometer: number = 0.621;
+        name: string;
+        speedMph: number;
 
+    constructor(name: string,speedMph: number,){
+        this.name = name
+        this.speedMph = speedMph
+    }
 
+    function getDaysToLocation(kilometersAway:number):number{
+        let milesAway: number = kilometersToMars * this.milesPerKilometer;
+        let hoursToLocation: number = milesAway / this.speedMph;
+        return hoursToLocation / 24;
+    }
+}
 
 
 // Create an instance of the class here:
-
+let spaceShuttle = new Spacecraft('Determination', 17500);
 
 
 // Move your output statements from part 3 here. Update the template literals use the
 // instance of the class.
-
-
+spaceShuttle.printDaysToLocation(new SpaceLocation('Mars', kilometersToMars));
+spaceShuttle.printDaysToLocation(new SpaceLocation('the Moon', kilometersToTheMoon));
 
 // Part 5: Export and Import the SpaceLocation Class
 // Add the required import statement BEFORE the part 1 concent.
